86b78d0d156eb55ad1d0bcfb7280a5f192bb39a5
ShahriarHA <shahriar08neub@gmail.com>
2026-04-07 12:40:36 +0600

    fastapi---uuid added

--- schema/product_schema.py
+++ schema/product_schema.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel, Field
 from typing import Annotated
-
+from uuid import UUID
 
 class Product(BaseModel):
-    id: str
+    id: UUID
     sku: Annotated[str,Field(min_length=6,max_length=36,title="SKU",description="Stock keeping Unit",examples=["SHAR-235GB-003","SAMS-253GB-003"])]
     name: str
