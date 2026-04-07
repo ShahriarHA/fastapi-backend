31f66b3135d08116c9e52da12f7ff35ae8a8cd29
ShahriarHA <shahriar08neub@gmail.com>
2026-04-07 09:13:52 +0600

    fastapi---Query,HTTPException,dynamicPage,pagination addded.

--- source_code/script2.py
+++ source_code/script2.py
@@ -11,7 +11,13 @@ def root():
 def list_products(
 name:str=Query(default=None, max_length=50, min_length=1, description="search by product name(case insensitive)"),
 sort_by_price:bool=Query(default=False,description="want to see products by its price 'max-lower' or 'lower-max'"),
-price_order:str = Query(default="asc",description="see all products by ascending order (need descending order just write 'desc')")
+price_order:str = Query(default="asc",description="see all products by ascending order (need descending order just write 'desc')"),
+limit: int = Query(
+    default=10, ge=1,le=100, description="number of items to return."
+),
+offset: int = Query(
+    default=0,ge=0,description="paggination offset!"
+)
 ):
     # n_ = name.strip().lower()
     products = get_all_data()
@@ -27,12 +33,18 @@ price_order:str = Query(default="asc",description="see all products by ascending
         is_reversed = (price_order.lower() == "desc")
         products = sorted(products,key=lambda p:p.get("price",0),reverse=is_reversed)
     
-    return {"total_products":len_products, "items":products}
-        
-        
-        
+    products = products[offset:offset+limit]
+    return {"total_products":len_products,"limit":limit, "items":products}
 
+@app.get("/products/{product_id}")
+def product_id(product_id:str):
+    products = get_all_data()
 
+    for product in products:
+        if product["id"] == product_id:
+            return product
+    raise HTTPException(status_code=404, detail="page not found!")
+    
 
 
 
